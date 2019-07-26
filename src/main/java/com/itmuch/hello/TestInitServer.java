77ba62f633a634105b6a93f313ecaa0252a80f30
wangshuai <wangshuai@songyekj.com>
2019-07-26 10:28:12 +0800

    配置gradle使用utf-8编译

--- src/main/java/com/itmuch/hello/TestInitServer.java
+++ src/main/java/com/itmuch/hello/TestInitServer.java
@@ -1,17 +1,17 @@
 package com.itmuch.hello;
 
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpServerCodec;
 
 import java.util.Date;
 
-public class TestInitServer extends ChannelInitializer {
+public class TestInitServer extends ChannelInitializer<SocketChannel> {
 
 
     @Override
-    protected void initChannel(Channel ch) throws Exception {
+    protected void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
         pipeline.addLast("httpServerCodec", new HttpServerCodec());
 
